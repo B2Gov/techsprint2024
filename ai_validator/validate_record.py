b3a5b2a7830f9793c7d11bca00063462420a39a2
Pablo Gonzalez Leiro <pagonzalez@itba.edu.ar>
2024-08-25 22:28:59 -0300

    Comment about the key file

--- ai_validator/validate_record.py
+++ ai_validator/validate_record.py
@@ -12,10 +12,25 @@ class OCDSValidatorAI():
     model_name = "gemini-1.5-flash-001"
 
     def __init__(self):
-        # Carga variables de entorno desde .env
+        # Carga variables de entorno desde .env. The .env should define
+        # the following variables: KEY_FILE, PROJECT, REGION
         load_dotenv()
 
         # Construye la ruta al archivo de clave de servicio
+        # The key file has this format:
+        # {
+        # "type": "service_account",
+        # "project_id": "<project_id>",
+        # "private_key_id": "<private_key_id>",
+        # "private_key": "<private_key>",
+        # "client_email": "<>",
+        # "client_id": "<>",
+        # "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        # "token_uri": "https://oauth2.googleapis.com/token",
+        # "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        # "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ai-svc-remote%40b2gov-ai.iam.gserviceaccount.com",
+        # "universe_domain": "googleapis.com"
+        # }
         key_file = os.path.dirname(__file__) + "\\" + os.getenv('KEY_FILE')
 
         # Crea objeto de credenciales desde el archivo de clave
